7130f93e25f6a85abebd3d114a3b6c1f25e4d56b
Armin Sabouri <armins88@gmail.com>
2023-12-21 16:23:52 -0500

    update libp2p lib and use gossipsub

--- src/main.rs
+++ src/main.rs
@@ -1,227 +1,122 @@
-use libp2p::{
-    core::upgrade,
-    floodsub::{Floodsub, FloodsubEvent, Topic},
-    futures::StreamExt,
-    identity,
-    mdns::{Mdns, MdnsEvent},
-    mplex,
-    noise::{Keypair, NoiseConfig, X25519Spec},
-    request_response::throttled::Event,
-    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
-    tcp::TokioTcpConfig,
-    NetworkBehaviour, PeerId, Transport,
-};
-use log::{error, info};
-use once_cell::sync::Lazy;
-use serde::{Deserialize, Serialize};
-use std::collections::HashSet;
-use tokio::{io::AsyncBufReadExt, sync::mpsc};
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+use futures::stream::StreamExt;
+use libp2p::{gossipsub, mdns, noise, swarm::NetworkBehaviour, swarm::SwarmEvent, tcp, yamux};
+use std::collections::hash_map::DefaultHasher;
+use std::error::Error;
+use std::hash::{Hash, Hasher};
+use std::time::Duration;
+use tokio::{io, io::AsyncBufReadExt, select};
+use tracing_subscriber::EnvFilter;
 
-static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
-static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
-static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("frost"));
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Ping {
-    message: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Pong {
-    message: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum EventResponseType {
-    Pong,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Response {
-    respose_type: EventResponseType,
-    data: Option<String>,
-    // peer id for who should recieve
-    receiver: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum EventRequestType {
-    Ping,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Request {
-    request_type: EventRequestType,
-    data: Option<String>,
-}
-
-#[derive(Debug)]
-enum EventType {
-    Response(Response),
-    Input(String),
-}
-
+// We create a custom network behaviour that combines Gossipsub and Mdns.
 #[derive(NetworkBehaviour)]
-struct FrostBehaviour {
-    floodsub: Floodsub,
-    mdns: Mdns,
-    #[behaviour(ignore)]
-    response_sender: mpsc::UnboundedSender<Response>,
-}
-
-impl NetworkBehaviourEventProcess<FloodsubEvent> for FrostBehaviour {
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        info!("Floodsub Event: {:?}", event);
-        match event {
-            FloodsubEvent::Message(msg) => {
-                // The following event is a Response
-                if let Ok(resp) = serde_json::from_slice::<Response>(&msg.data) {
-                    if resp.receiver == PEER_ID.to_string() {
-                        info!("Got a pong response from {}:", msg.source);
-                    }
-                } 
-                // The following event is a Request
-                else if let Ok(req) = serde_json::from_slice::<Request>(&msg.data) {
-                    if req.request_type == EventRequestType::Ping {
-                        info!("Got a ping request from {}:", msg.source);
-                        respond_with_pong(self.response_sender.clone(), msg.source.to_string());
-                    }
-                }
-            }
-            _ => (),
-        }
-    }
-}
-
-fn respond_with_pong(sender: mpsc::UnboundedSender<Response>, receiver: String) {
-    tokio::spawn(async move {
-        let response: Response = Response {
-            respose_type: EventResponseType::Pong,
-            data: None,
-            receiver,
-        };
-        sender.send(response).expect("can send response");
-    });
-}
-
-impl NetworkBehaviourEventProcess<MdnsEvent> for FrostBehaviour {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        match event {
-            MdnsEvent::Discovered(discovered_list) => {
-                for (peer, _addr) in discovered_list {
-                    self.floodsub.add_node_to_partial_view(peer);
-                }
-            }
-            MdnsEvent::Expired(expired_list) => {
-                for (peer, _addr) in expired_list {
-                    if !self.mdns.has_node(&peer) {
-                        self.floodsub.remove_node_from_partial_view(&peer);
-                    }
-                }
-            }
-        }
-    }
+struct MyBehaviour {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::tokio::Behaviour,
 }
 
 #[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
 
-    info!("Peer Id: {}", PEER_ID.clone());
-    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_quic()
+        .with_behaviour(|key| {
+            // To content-address message, we can take the hash of message and use it as an ID.
+            let message_id_fn = |message: &gossipsub::Message| {
+                let mut s = DefaultHasher::new();
+                message.data.hash(&mut s);
+                gossipsub::MessageId::from(s.finish().to_string())
+            };
 
-    let auth_keys = Keypair::<X25519Spec>::new()
-        .into_authentic(&KEYS)
-        .expect("can create auth keys");
+            // Set a custom gossipsub configuration
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .build()
+                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
 
-    let transp = TokioTcpConfig::new()
-        .upgrade(upgrade::Version::V1)
-        // XX Handshake pattern, IX exists as well and IK - only XX currently provides interop with other libp2p impls
-        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
-        .multiplex(mplex::MplexConfig::new())
-        .boxed();
+            // build a gossipsub network behaviour
+            let gossipsub = gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(key.clone()),
+                gossipsub_config,
+            )?;
 
-    let mut behaviour = FrostBehaviour {
-        floodsub: Floodsub::new(PEER_ID.clone()),
-        mdns: Mdns::new(Default::default())
-            .await
-            .expect("can create mdns"),
-        response_sender,
-    };
-
-    behaviour.floodsub.subscribe(TOPIC.clone());
-
-    let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
-        .executor(Box::new(|fut| {
-            tokio::spawn(fut);
-        }))
+            let mdns =
+                mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
+            Ok(MyBehaviour { gossipsub, mdns })
+        })?
+        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
         .build();
 
-    let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+    // Create a Gossipsub topic
+    let topic = gossipsub::IdentTopic::new("test-net");
+    // subscribes to our topic
+    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
-    Swarm::listen_on(
-        &mut swarm,
-        "/ip4/0.0.0.0/tcp/0"
-            .parse()
-            .expect("can get a local socket"),
-    )
-    .expect("swarm can be started");
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
 
+    // Listen on all interfaces and whatever port the OS assigns
+    swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+
+    // Kick it off
     loop {
-        let evt = {
-            tokio::select! {
-                line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
-                response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
-                event = swarm.select_next_some() => {
-                    info!("Unhandled Swarm Event: {:?}", event);
-                    None
-                },
-            }
-        };
-
-        if let Some(event) = evt {
-            log::info!("Event: {:?}", event);
-            match event {
-                EventType::Response(resp) => {
-                    let json = serde_json::to_string(&resp).expect("can jsonify response");
-                    swarm
-                        .behaviour_mut()
-                        .floodsub
-                        .publish(TOPIC.clone(), json.as_bytes());
+        select! {
+            Ok(Some(line)) = stdin.next_line() => {
+                match line.as_str() {
+                    "ls p" => {
+                        println!("Connected peers:");
+                        for peer in swarm.behaviour_mut().gossipsub.all_peers() {
+                            println!("{:?}", peer);
+                        }
+                    }
+                    _ => println!("Sending message: {}", line),
                 }
-                EventType::Input(line) => match line.as_str() {
-                    "ls p" => handle_list_peers(&mut swarm).await,
-                    cmd if cmd.starts_with("ping") => handle_ping(&mut swarm).await,
-                    _ => error!("unknown command"),
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line.as_bytes()) {
+                    println!("Publish error: {e:?}");
+                }
+            }
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
                 },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => println!(
+                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                        String::from_utf8_lossy(&message.data),
+                    ),
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Local node is listening on {address}");
+                }
+                _ => {}
             }
         }
     }
-}
-
-async fn handle_ping(swarm: &mut Swarm<FrostBehaviour>) {
-    info!("Sending Ping");
-    let request = Request {
-        request_type: EventRequestType::Ping,
-        data: None,
-    };
-    let json = serde_json::to_string(&request).expect("can jsonify request");
-
-    println!("json: {}", json);
-    swarm
-        .behaviour_mut()
-        .floodsub
-        .publish(TOPIC.clone(), json.as_bytes());
-}
-
-async fn handle_list_peers(swarm: &mut Swarm<FrostBehaviour>) {
-    info!("Discovered Peers:");
-    let nodes = swarm.behaviour().mdns.discovered_nodes();
-    let mut unique_peers = HashSet::new();
-    for peer in nodes {
-        unique_peers.insert(peer);
-    }
-    unique_peers.iter().for_each(|p| info!("{}", p));
 }
